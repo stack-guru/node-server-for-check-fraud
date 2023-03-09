d891cdbcb730ffb48a3f9e48e41165e217bc82fe
Oliver <GoldenHand0302@gmail.com>
2023-03-09 17:41:28 +0100

    change port

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 
-const { API_PORT } = process.env;
-const port = API_PORT || 4001;
+const port = process.env.PORT || 4001;
 
 // server listening 
 server.listen(port, () => {
