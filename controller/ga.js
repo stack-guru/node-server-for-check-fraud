4d83c58a358604b03b5fef41eec100ffde46ecc9
Oliver <GoldenHand0302@gmail.com>
2023-03-21 21:10:30 +0100

    update ga

--- controller/ga.js
+++ controller/ga.js
@@ -4,69 +4,77 @@ const propertyId = process.env.GA_PROPERTY_ID
 const analyticsDataClient = new BetaAnalyticsDataClient();
 
 exports.getWeekData = async (req, res) => {
-    const [response] = await analyticsDataClient.runReport({
-        property: `properties/${propertyId}`,
-        dateRanges: [
-            {
-                startDate: '7daysAgo',
-                endDate: 'today'
-            }
-        ],
-        metrics: [
-            {
-                name: 'activeUsers'
-            },
-            {
-                name: 'totalUsers'
-            },
-            {
-                name: 'newUsers'
-            },
-            {
-                name: 'userEngagementDuration'
-            },
-            {
-                name: 'eventCount'
-            }
-        ],
-        dimensions: [
-            {
-                name: 'date'
-            }
-        ],
-    });
-
-    if (response.rowCount) {
-        res.status(201).json(response.rows)
-    } else {
-        res.status(201).json([])
+    try {
+        const [response] = await analyticsDataClient.runReport({
+            property: `properties/${propertyId}`,
+            dateRanges: [
+                {
+                    startDate: '7daysAgo',
+                    endDate: 'today'
+                }
+            ],
+            metrics: [
+                {
+                    name: 'activeUsers'
+                },
+                {
+                    name: 'totalUsers'
+                },
+                {
+                    name: 'newUsers'
+                },
+                {
+                    name: 'userEngagementDuration'
+                },
+                {
+                    name: 'eventCount'
+                }
+            ],
+            dimensions: [
+                {
+                    name: 'date'
+                }
+            ],
+        });
+    
+        if (response.rowCount) {
+            res.status(201).json(response.rows)
+        } else {
+            res.status(201).json([])
+        }
+    } catch(err) {
+        res.status(403).json(err)
     }
 }
 
 exports.getGraphData = async (req, res) => {
-    const [response] = await analyticsDataClient.runReport({
-        property: `properties/${propertyId}`,
-        dateRanges: [
-            {
-                startDate: '7daysAgo',
-                endDate: 'today'
-            }
-        ],
-        metrics: [
-            {
-                name: 'totalUsers'
-            },
-        ],
-        dimensions: [
-            {
-                name: 'date'
-            }
-        ],
-    });
-
-    if (response.rowCount) {
-        res.status(201).json(response.rows)
-    } else {
-        res.status(201).json([])
+    try {
+        const [response] = await analyticsDataClient.runReport({
+            property: `properties/${propertyId}`,
+            dateRanges: [
+                {
+                    startDate: '7daysAgo',
+                    endDate: 'today'
+                }
+            ],
+            metrics: [
+                {
+                    name: 'totalUsers'
+                },
+            ],
+            dimensions: [
+                {
+                    name: 'date'
+                }
+            ],
+        });
+    
+        if (response.rowCount) {
+            res.status(201).json(response.rows)
+        } else {
+            res.status(201).json([])
+        }
+    } catch(err) {
+        res.startDate(403).json(err)
     }
 }
